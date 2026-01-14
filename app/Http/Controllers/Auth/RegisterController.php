e05301892fd55b022fcd10b2f458acc62d40af05
habi <shb8002@gmail.com>
2026-01-15 00:03:23 +0900

    [feat] 세션 방식 로그인

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -20,5 +20,7 @@ class RegisterController extends Controller
             ...$request->validated(),
             'password' => Hash::make($request->password),
         ]);
+
+        //auth()->login($user);
     }
 }
