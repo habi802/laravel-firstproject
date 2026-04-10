4b00eb512be65daacbbca03f638333da2bc832d9
habi <shb8002@gmail.com>
2026-04-10 19:05:25 +0900

    [feat] 피드 - 컬렉션 매크로

--- app/Providers/PaginateServiceProvider.php
+++ app/Providers/PaginateServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 class PaginateServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,15 @@ class PaginateServiceProvider extends ServiceProvider
     {
         Paginator::defaultView('vendor/pagination/default');
         Paginator::defaultSimpleView('vendor\pagination/simple-default');
+
+        Collection::macro('paginate', function (int $perPage, int $currentPage, array $options = []) {
+            return app(LengthAwarePaginator::class, [
+                'items' => $this->forPage($currentPage, $perPage),
+                'total' => $this->count(),
+                'perPage' => $perPage,
+                'currentPage' => $currentPage,
+                'options' => $options,
+            ]);
+        });
     }
 }
