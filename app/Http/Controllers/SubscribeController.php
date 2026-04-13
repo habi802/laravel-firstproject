744e4d17fb070a12037a989ab0f5f52c87135bc2
habi <shb8002@gmail.com>
2026-04-14 01:20:03 +0900

    [feat] 메일

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\SubscribeRequest;
 use App\Models\Blog;
 use App\Events\Subscribed;
 use App\Http\Requests\UnsubscribeRequest;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Subscribed as SubscribedMailable;
 
 class SubscribeController extends Controller
 {
@@ -18,6 +20,15 @@ class SubscribeController extends Controller
         $user->subscriptions()->attach($blog->id);
 
         event(new Subscribed($user, $blog));
+
+        Mail::to($blog->user)
+            //->send(new SubscribedMailable($user, $blog));
+            // ->queue(
+            //     (new SubscribedMailable($user, $blog))->onQueue('emails')
+            // );
+            ->send(
+                (new SubscribeMailable($user, $blog))->onQueue('emails')
+            );
 
         return back();
     }
