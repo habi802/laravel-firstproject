2f192ca610fdc2ecf58d1bc821fad6d2902f04dd
habi <shb8002@gmail.com>
2026-04-14 18:18:52 +0900

    [feat] 알림 Notifiable

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -9,6 +9,7 @@ use App\Events\Subscribed;
 use App\Http\Requests\UnsubscribeRequest;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\Subscribed as SubscribedMailable;
+use App\Notifications\Subscribed as SubscribedNotification;
 
 class SubscribeController extends Controller
 {
@@ -21,14 +22,15 @@ class SubscribeController extends Controller
 
         event(new Subscribed($user, $blog));
 
-        Mail::to($blog->user)
-            //->send(new SubscribedMailable($user, $blog));
-            // ->queue(
-            //     (new SubscribedMailable($user, $blog))->onQueue('emails')
-            // );
-            ->send(
-                (new SubscribedMailable($user, $blog))->onQueue('emails')
-            );
+        // Mail::to($blog->user)
+        //     //->send(new SubscribedMailable($user, $blog));
+        //     // ->queue(
+        //     //     (new SubscribedMailable($user, $blog))->onQueue('emails')
+        //     // );
+        //     ->send(
+        //         (new SubscribedMailable($user, $blog))->onQueue('emails')
+        //     );
+        $blog->user->notify(new SubscribedNotification($user, $blog));
 
         return back();
     }
