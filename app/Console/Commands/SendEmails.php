60fee894cf145827334453a7ddc939266fae2a6d
habi <shb8002@gmail.com>
2026-04-27 17:51:50 +0900

    [feat] 메일 전송 커맨드 생성

--- app/Console/Commands/SendEmails.php
+++ app/Console/Commands/SendEmails.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Advertisement;
+use App\Models\User;
+use App\Models\Post;
 
 class SendEmails extends Command
 {
@@ -11,20 +15,44 @@ class SendEmails extends Command
      *
      * @var string
      */
-    protected $signature = 'app:send-emails';
+    protected $signature = 'mail:send {--Q|queue=default : The names of the queues to send emails}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Send advertisement emails to users';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $queue = $this->option('queue');
+
+        $this->sendEmails($queue);
+
+        return 0;
+    }
+
+    private function sendEmails(string $queue)
+    {
+        $posts = $this->posts();
+
+        $this->users()->each(fn (User $user) => Mail::to($user)->send(
+            (new Advertisement($posts))->onQueue($queue)
+        ));
+    }
+
+    private function users()
+    {
+        return User::all();
+        // return User::verified()->get();
+    }
+
+    private function posts()
+    {
+        return Post::latest()->limit(5)->get();
     }
 }
---
+++ app/Mail/Advertisement.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Database\Eloquent\Collection;
+
+class Advertisement extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public readonly Collection $posts)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: '[라라벨] 라라벨 커뮤니티의 최신글 살펴보기',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.advertisement',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
