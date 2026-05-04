f10546ca787763df35221e1b8d566eb0440f323f
habi <shb8002@gmail.com>
2026-05-04 19:16:56 +0900

    [fix] 토큰 수정

--- app/Http/Controllers/Auth/TokenController.php
+++ app/Http/Controllers/Auth/TokenController.php
@@ -23,7 +23,7 @@ class TokenController extends Controller
      */
     public function create()
     {
-        return view('token.create', [
+        return view('tokens.create', [
             'abilities' => Ability::cases()
         ]);
     }
