f92ab4f676283e3fbb593318775a11a9195ea82e
habi <shb8002@gmail.com>
2026-06-09 03:58:03 +0900

    [feat] jwt 로그인 컨트롤러

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,22 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('web')
                 ->group(base_path(('routes/auth.php')));
+
+            // JWT
+            Route::withoutMiddleware('web')->middleware('api')->group(function () {
+                Route::controller(\App\Http\Controllers\Auth\JwtLoginController::class)->group(function () {
+                    Route::name('jwt.')->prefix('jwt')->group(function () {
+                        Route::post('login', 'store')
+                             ->name('login');
+                        Route::middleware('auth:api')->group(function () {
+                            Route::put('refresh', 'update')
+                                 ->name('refresh');
+                            Route::delete('logout', 'destroy')
+                                 ->name('logout');
+                        });
+                    });
+                });
+            });
         });
     }
 }
